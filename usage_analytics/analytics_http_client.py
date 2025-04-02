d39f064e0b0b42e514df9b66e43f35f1c73308bb
Liam, SB Hoo <shibinhoo@gmail.com>
2025-04-02 15:52:44 +0200

    Also override httpx client stream

--- usage_analytics/analytics_http_client.py
+++ usage_analytics/analytics_http_client.py
@@ -16,15 +16,24 @@ class AnalyticsHttpClient(httpx.Client):
         if headers is None:
             headers = {}
 
-        # Add the analytics headers
+        kwargs["headers"] = self._add_analytics_headers(headers)
+
+        # Call the original request method
+        return super().request(method, url, *args, **kwargs)
+
+    def stream(self, method, url, *args, **kwargs):
+        headers = kwargs.get("headers", {})
+        if headers is None:
+            headers = {}
+
+        kwargs["headers"] = self._add_analytics_headers(headers)
+
+        return super().stream(method, url, *args, **kwargs)
+
+    def _add_analytics_headers(self, headers: dict):
         for header_name, get_value_func in ANALYTICS_TO_TRACK:
             if header_name == "X-Module-Name":
                 headers[header_name] = self.module_name
             elif get_value_func is not None:
                 headers[header_name] = get_value_func()
-
-        print(f"headers: {headers}")
-        kwargs["headers"] = headers
-
-        # Call the original request method
-        return super().request(method, url, *args, **kwargs)
+        return headers
